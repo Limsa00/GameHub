679320c120f22d7aaa9cf157d621d0fa3d5b7e48
Limsa00 <bennis.salim@gmail.com>
2021-09-07 11:02:14 +0200

    dynamic routes for each game

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = require('./my_modules/routes');
 const port = 3000;
+const games = require ('./games.json');
+
 
 const server = express();
 server.set('view engine', 'ejs');
@@ -8,6 +10,8 @@ server.set('view engine', 'ejs');
 server.use(router);
 
 server.use(express.static(__dirname + '/public'));
+
+server.locals.games = games;
 
 server.listen(port, () => {
     console.log(`Le serveur tourne sur http://localhost:${port}`);
--- my_modules/routes.js
+++ my_modules/routes.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const gamesData = require ('../games.json');
+
 
 router.get('/', (req,res) => {
     res.render('index')
 });
 
-router.get('/game/fourchette', (req,res) => {
-    res.render('fourchette')
-});
+router.get('/game/:name', (req,res) => {
+    const name = req.params.name;
 
-router.get('/game/diceRoller', (req,res) => {
-    res.render('diceRoller')
+    let game
+
+    for (let index = 0; index < gamesData.length; index++) {
+        const gameData = gamesData[index];
+
+        if (gameData.name === name) {
+            game = gameData;
+        };
+    };
+
+    if (typeof game !== 'undefined') {
+        res.render(game.name);
+    }
+    else {
+        res.status(404).send('erreur');
+    }
 });
 
 module.exports = router;
